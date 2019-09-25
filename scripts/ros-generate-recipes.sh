4473787e8eba21fe6f345e650fcda341a1c3a012
Martin Jansa <martin.jansa@lge.com>
2019-09-25 06:27:47 -0700

    DOWN ros-generate-cache.sh: Split cache.yaml generation off into own script
    Also retain copies of the rosdep platform package mapping files.
    
    NB. Correct operation of --only require superflore v0.3.0+hk7

--- scripts/ros-generate-recipes.sh
+++ scripts/ros-generate-recipes.sh
@@ -1,38 +1,28 @@
 # /bin/sh (deliberately no !#)
 #
 # Usage: cd meta-ros
-#        [SUPERFLORE_GEN_OE_RECIPES=/path/to/superflore-gen-oe-recipes] sh scripts/ros-generate-recipes.sh CMD ARGS ...
+#        [SUPERFLORE_GEN_OE_RECIPES=/path/to/superflore-gen-oe-recipes] sh scripts/ros-generate-recipes.sh ROS_DISTRO [ROS_PKG1 ROS_PKG2 ...]
 #        (current branch is now superflore/DATETIME)
 #            or
 #        sh scripts/ros-generate-recipes.sh --version
 #
-# The recognized CMD ARGS are:
-#
-#         from-release ROS_DISTRO YYYYMMDD PATH-TO-LOCAL-ROSDISTRO ROSDISTRO-COMMIT
-#             - Generate all the recipes for the YYYYMMDD release of ROS_DISTRO from the specified commit of a local
-#               ros/rosdistro.git . If running prior to first release of ROS_DISTRO, specify "none" for YYYYMMDD. NB. The release
-#               date might not match the commit timestamp of ROSDISTRO-COMMIT; however, the commit timestamp is used for the
-#               DATETIME when forming the name of the created branch.
-#
-#         single PATH-TO-LOCAL-ROSDISTRO ROSDISTRO-COMMIT
-#             - Generate the recipe changed by ROSDISTRO-COMMIT. The ROS_DISTRO value is inferred. (Not yet implemented.)
-#
-#         regen ROS_DISTRO [ROS_PKG1 ROS_PKG2 ...]
-#             - Re-generate the recipes for the specified packages from the existing ROS_DISTRO/cache.yaml. If no packages are
-#               specified, re-generate all of the recipes. (Not yet implemented.)
+#        Generate (or re-generate) recipes for the ROS_DISTRO from the existing ROS_DISTRO/cache.yaml. If no ROS_PKG arguments are given,
+#        then all of the recipes for ROS_DISTRO are generated.
 #
 # This script will abort if Git detects any uncommitted modifications, eg, from a previous run that did not complete or untracked
 # files (which would otherwise appear in files/ROS_DISTRO/superflore-change-summary.txt).
 #
 # Copyright (c) 2019 LG Electronics, Inc.
 
-SCRIPT_NAME="ros-generate-recipes"
-SCRIPT_VERSION="1.0.0"
+readonly SCRIPT_NAME="ros-generate-recipes"
+readonly SCRIPT_VERSION="1.0.0"
+
+# Files under ros/rosdistro/rosdep that we care about. Keep in sync with setting in ros-generate-cache.sh .
+readonly ROSDEP_YAML_BASENAMES="base python ruby"
 
 usage() {
     echo "Usage: cd meta-ros"
-    echo "       [SUPERFLORE_GEN_OE_RECIPES=/path/to/superflore-gen-oe-recipes] sh scripts/$SCRIPT_NAME.sh \\"
-    echo "           from-release ROS_DISTRO YYYYMMDD PATH-TO-LOCAL-ROSDISTRO ROSDISTRO-COMMIT"
+    echo "       [SUPERFLORE_GEN_OE_RECIPES=/path/to/superflore-gen-oe-recipes] sh scripts/$SCRIPT_NAME.sh ROS_DISTRO [ROS_PKG1 ROS_PKG2 ...]"
     echo "               or"
     echo "       sh scripts/$SCRIPT_NAME.sh --version"
     exit 1
@@ -43,7 +33,7 @@ if [ "$1" = "--version" ]; then
     exit
 fi
 
-[ $# -ne 5 ] && usage
+[ $# -eq 0 ] && usage
 
 [ -z "$SUPERFLORE_GEN_OE_RECIPES" ] && SUPERFLORE_GEN_OE_RECIPES=$(which superflore-gen-oe-recipes)
 if [ -z "$SUPERFLORE_GEN_OE_RECIPES" ]; then
@@ -51,10 +41,9 @@ if [ -z "$SUPERFLORE_GEN_OE_RECIPES" ]; then
     exit
 fi
 
-[ $1 != "from-release" ] && usage
-
-ROS_DISTRO=$2
-# ROS_VERSION and ROS_PYTHON_VERSION must be in the environment as they appear in "conditional" attributes.
+ROS_DISTRO=$1
+# ROS_VERSION and ROS_PYTHON_VERSION must be in the environment as they appear in "conditional" attributes. Keep this block in
+# sync with the one in ros-generate-cache.sh .
 case $ROS_DISTRO in
     "kinetic"|"melodic")
         export ROS_VERSION="1"
@@ -71,20 +60,10 @@ case $ROS_DISTRO in
         ;;
 esac
 
-ROS_DISTRO_RELEASE_DATE=$3
-case $ROS_DISTRO_RELEASE_DATE in
-    none|[2-9][0-9][0-9][0-9][0-1][0-9][0-3][0-9])
-        : OK
-        ;;
-
-    *)  echo "ABORT: ROS_DISTRO_RELEASE_DATE not YYYYMMDD or 'none': '$3'"
-        exit 1
-        ;;
-esac
-
-if [ ! -d $4 ]; then
-    echo "ABORT: '$4' not found"
-    exit 1
+only_option=""
+if [ $# -gt 1 ]; then
+    shift
+    only_option="--only $*"
 fi
 
 if [ -n "$(git status --porcelain=v1)" ]; then
@@ -92,19 +71,54 @@ if [ -n "$(git status --porcelain=v1)" ]; then
     exit 1
 fi
 
-cd $4
-path_to_rosdistro=$PWD
-rev=$(git rev-list -1 $5) || exit 1
-commit_timestamp=$(git log -1 --date=iso-strict --format=%cd $rev)
-# Associate the upcoming superflore run with the commit date of <ROSDISTRO-COMMIT>.
-export SUPERFLORE_GENERATION_DATETIME=$(date +%Y%m%d%H%M%S --utc -d $commit_timestamp)
-unset commit_timestamp
-cd - > /dev/null
-
-if [ -n "$(git branch --list superflore/$SUPERFLORE_GENERATION_DATETIME)" ]; then
-    echo "ABORT: Branch 'superflore/$SUPERFLORE_GENERATION_DATETIME' already exists"
+if [ ! -f files/$ROS_DISTRO/cache.yaml ]; then
+    echo "ABORT: files/$ROS_DISTRO/cache.yaml doesn't exist -- run ros-generate-cache.sh to create it"
     exit 1
 fi
+
+if [ ! -f files/$ROS_DISTRO/index-v4.yaml ]; then
+    echo "ABORT: files/$ROS_DISTRO/index-v4.yaml doesn't exist -- run ros-generate-cache.sh to create it"
+    exit 1
+fi
+
+set -- $(head -n 1 files/$ROS_DISTRO/cache.yaml)
+ROS_DISTRO_RELEASE_DATE=$3
+ROS_ROSDISTRO_COMMIT=$4
+ROS_ROSDISTRO_COMMIT_DATETIME=$5
+
+if [ -z "$ROS_DISTRO_RELEASE_DATE" -o -z "$ROS_ROSDISTRO_COMMIT" -o -z "$ROS_ROSDISTRO_COMMIT_DATETIME" ] ; then
+    echo "ABORT: files/$ROS_DISTRO/cache.yaml should start with '# $ROS_DISTRO/cache.yaml' and 3 space separated values:"
+    echo "       ROS_DISTRO_RELEASE_DATE, ROS_ROSDISTRO_COMMIT, ROS_ROSDISTRO_COMMIT_DATETIME. Run ros-generate-cache.sh to"
+    echo "       re-create it."
+    exit 1
+fi
+
+# Keep this block in sync with the one in ros-generate-cache.sh .
+case $ROS_DISTRO_RELEASE_DATE in
+    pre-release|[2-9][0-9][0-9][0-9][0-1][0-9][0-3][0-9])
+        : OK
+        ;;
+
+    *)  echo "ABORT: ROS_DISTRO_RELEASE_DATE not YYYYMMDD or 'pre-release': '$ROS_DISTRO_RELEASE_DATE'"
+        exit 1
+        ;;
+esac
+
+
+abort=false
+for f in $ROSDEP_YAML_BASENAMES; do
+    ff=$(sed -n "\@^yaml file:/.*/$f.yaml\$@ s@.*file://@@p" /etc/ros/rosdep/sources.list.d/20-default.list)
+    # -q reports when they differ.
+    diff -q files/$ROS_DISTRO/rosdep/$f.yaml $ff || abort=true
+done
+unset f ff
+if $abort; then
+    echo "ABORT: The files pointed to by /etc/ros/rosdep/sources.list.d/20-default.list must match those under files/$ROS_DISTRO/rosdep"
+    exit 1
+fi
+unset abort
+
+# Done checking things.
 
 skip_keys_option=""
 ros1_lisp_packages="euslisp geneus genlisp roslisp actionlib_lisp cl_tf cl_tf2 cl_transforms cl_transforms_stamped cl_urdf cl_utils roslisp_common roslisp_utilities rosemacs ros_emacs_utils roslisp_repl slime_ros slime_wrapper"
@@ -123,62 +137,44 @@ case $ROS_DISTRO in
         ;;
 esac
 
-only_option=""
-# XXX Eventually:
-if [ $1 = "regen" ]; then
-    shift 2
-    [ $# -gt 0 ] && only_option="--only $*"
-fi
+# Keep this block in sync with the one in ros-generate-cache.sh .
+echo "Running 'rosdep update'"
+rosdep update || { echo "ABORT: 'rosdep update' failed"; exit 1; }
+echo "'rosdep update' finished"
 
 tmpdir=$(mktemp -t -d ros-generate-recipes-XXXXXXXX)
 trap "rm -rf $tmpdir" 0
 
-# Create a directory tree under $tmpdir with the contents of ros/rosdistro.git at commit $rev.
-cd $path_to_rosdistro
-git archive $rev | tar -C $tmpdir -xf -
-cd - > /dev/null
-
-# Create $tmpdir/$ROS_DISTRO-cache.yaml.gz .
-cd $tmpdir
-
-# XXX Fix up a package that's been renamed. Only needed if generating from a commit prior to 2019-09-05.
-false && \
-sed -i -e 's/micro-xrce-dds-agent:/microxrcedds_agent:/' \
-       -e 's@https://github.com/micro-ROS/Micro-XRCE-DDS-Agent-release.git@https://github.com/micro-ROS/microxrcedds_agent-release.git@' \
-       $ROS_DISTRO/distribution.yaml
-
-rosdistro_build_cache --preclean --ignore-local $tmpdir/index-v4.yaml $ROS_DISTRO
-
-# Fixup the index there to use the newly created $ROS_DISTRO-cache.yaml.gz .
-sed -i -e "/$ROS_DISTRO-cache.yaml.gz/ s@: .*\$@: file://$tmpdir/$ROS_DISTRO-cache.yaml.gz@" index-v4.yaml
-cd - > /dev/null
+cp files/$ROS_DISTRO/index-v4.yaml $tmpdir/
+sed -i -e "/$ROS_DISTRO-cache.yaml.gz/ s@: .*\$@: file://$PWD/files/$ROS_DISTRO/cache.yaml@" $tmpdir/index-v4.yaml
 
 # Tell superflore to use this index instead of the upstream one.
 export ROSDISTRO_INDEX_URL="file://$tmpdir/index-v4.yaml"
-
-rosdep update || { echo "ABORT: 'rosdep update' failed"; exit 1; }
+# Have it use this date instead of "now".
+export SUPERFLORE_GENERATION_DATETIME="$ROS_ROSDISTRO_COMMIT_DATETIME"
 
 before_commit=$(git rev-list -1 HEAD)
 $SUPERFLORE_GEN_OE_RECIPES --dry-run --ros-distro $ROS_DISTRO --output-repository-path . --upstream-branch HEAD \
                             $skip_keys_option $only_option
 
 after_commit=$(git rev-list -1 HEAD)
-if [ $after_commit != $before_commit ]; then
-    # Identify how the files were generated so that they can be reused.
-    sed -i -e "1 s/\$/ $ROS_DISTRO_RELEASE_DATE $rev/" files/$ROS_DISTRO/cache.yaml
-    sed -i -e "1 s/\$/ $ROS_DISTRO_RELEASE_DATE $rev/" files/$ROS_DISTRO/cache.diffme
+if [ $after_commit != $before_commit -a -z "$only_option" ]; then
+    # XXX Pretend that superflore has already been fixed not to generate files/ROS_DISTRO/cache.* and delete the comment line it
+    # prepends. (The line ends with the filename.)
+    sed -i -e '/^#.*cache[.]yaml$/ d' files/$ROS_DISTRO/cache.yaml
+    sed -i -e '/^#.*cache[.]diffme$/ d' files/$ROS_DISTRO/cache.diffme
     git add files/$ROS_DISTRO/cache.yaml files/$ROS_DISTRO/cache.diffme
 
     generated="conf/ros-distro/include/$ROS_DISTRO/generated-ros-distro.inc"
-    [ $ROS_DISTRO_RELEASE_DATE = "none" ] && ROS_DISTRO_RELEASE_DATE=""
+    [ $ROS_DISTRO_RELEASE_DATE = "pre-release" ] && ROS_DISTRO_RELEASE_DATE=""
     cat <<! >> $generated
 
 # From the release announcement or the last field of the "release-ROS_DISTRO-YYYYMMDD" tag for the release in
 # https://github.com/ros2/ros2/releases. Prior to the first release of a ROS_DISTRO, it is set to "".
 ROS_DISTRO_RELEASE_DATE = "$ROS_DISTRO_RELEASE_DATE"
 
-# The commit of rosdistro/$ROS_DISTRO/distribution.yaml from which the recipes were generated.
-ROS_SUPERFLORE_GENERATION_COMMIT = "$rev"
+# The commit of ros/rosdistro/$ROS_DISTRO/distribution.yaml from which the recipes were generated.
+ROS_SUPERFLORE_GENERATION_COMMIT = "$ROS_ROSDISTRO_COMMIT"
 !
     git add $generated
     git commit --amend -q -C HEAD
